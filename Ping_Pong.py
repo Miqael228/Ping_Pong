e1527ff9d471bf50694d27e6be8f94d240680d7f
Michael <130777385+Miqael228@users.noreply.github.com>
2023-04-28 19:12:42 +0300

    Update Ping_Pong.py

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+from time import time as timer
 width = 700
 height = 500
 clock = time.Clock()
@@ -7,10 +8,13 @@ win = display.set_mode((width, height))
 display.set_caption('Ping-Pong')
 finish = False
 Game = True
+player_1 = 0
+player_2 = 0
+font.init()
 FPS = 120
 background = (102, 255, 102)
-
-
+pause = False
+timer1 = timer()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, rect_x, rect_y, im, speed, w, h):
@@ -48,11 +52,11 @@ class Ball(GameSprite):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
         
+font1 = font.Font(None, 45)
+player_2_lose = font1.render('Player 2 Win!', True, (250, 0, 0))
+player_1_lose = font1.render('Player 1 Win!', True, (250, 0, 0))
 
-
-
-
-ball = Ball(350, 250, 'Ball.png', 2, sprite_side, sprite_side)
+ball = Ball(200, 250, 'Ball.png', 2, sprite_side, sprite_side)
 player_left = Player(65, 0, 'Stick.png', 3, 15, 100)
 player_right = Player(600, 0, 'Stick.png', 3, 15, 100)
 
@@ -68,22 +72,43 @@ player_right = Player(600, 0, 'Stick.png', 3, 15, 100)
 
 
 while Game:
-    win.fill(background)
-    player_left.update_L()
-    player_right.update_R()
-    player_left.reset()
-    player_right.reset()
-    ball.update()
-    ball.reset()
-    if sprite.collide_rect(player_left, ball) or sprite.collide_rect(player_right, ball):
-        ball.speed_x *= -1
-        ball.speed_y *= 1
-    if ball.rect.y > height - sprite_side or ball.rect.y < 0:
-        ball.speed_y *= -1
-
+    if not finish:
+        win.fill(background)
+        player_left.update_L()
+        player_right.update_R()
+        player_left.reset()
+        player_right.reset()
+        ball.update()
+        ball.reset()
+        if sprite.collide_rect(player_left, ball) or sprite.collide_rect(player_right, ball):
+            ball.speed_x *= -1
+            ball.speed_y *= 1
+        if ball.rect.y > height - sprite_side or ball.rect.y < 0:
+            ball.speed_y *= -1
+        if ball.rect.x > width:
+            win.blit(player_1_lose, (250, 235))
+            print('Player 1 Win!')
+            player_1 += 1
+            finish = True
+            timer1 = timer()
+        elif ball.rect.x < 0:
+            print('Player 2 Win!')
+            win.blit(player_2_lose, (250, 235))
+            player_2 += 1
+            finish = True
+            timer1 = timer()
 
     for e in event.get():
         if e.type == QUIT:
             Game = False
+    if finish:
+        
+        if timer() - timer1 > 5:
+            ball.rect.x = 200
+            ball.rect.y = 250
+            player_left.rect.x = 65
+            player_right.rect.x = 600
+            finish = False
+
     display.update()
     clock.tick(FPS)
